9fadda54c8de757640e37516fc3413dee9a75c3f
herediaalejo <herediaalejodeveloper@gmail.com>
2025-02-12 22:12:03 -0300

    ejercicio 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,22 +3,17 @@ import Button from "./components/Button";
 import Statistics from "./components/Statistics";
 
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const addGoodFeedback = () => {
-    setGood(good + 1);
-  };
+  const allFeedbacks = good + neutral + bad;
+  const average = allFeedbacks > 0 ? (good - bad) / allFeedbacks : 0;
+  const posPercentage = allFeedbacks > 0 ? (good * 100) / allFeedbacks : 0;
 
-  const addNeutralFeedback = () => {
-    setNeutral(neutral + 1);
-  };
-
-  const addBadFeedback = () => {
-    setBad(bad + 1);
-  };
+  const addGoodFeedback = () => setGood(good + 1);
+  const addNeutralFeedback = () => setNeutral(neutral + 1);
+  const addBadFeedback = () => setBad(bad + 1);
 
   return (
     <div>
@@ -27,7 +22,14 @@ const App = () => {
       <Button handleClick={addNeutralFeedback} text={"neutral"} />
       <Button handleClick={addBadFeedback} text={"bad"} />
       <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={allFeedbacks}
+        average={average}
+        posPercentage={posPercentage}
+      />
     </div>
   );
 };
--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,9 +1,12 @@
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad, all, average, posPercentage }) => {
   return (
     <ul>
       <li>good {good}</li>
       <li>neutral {neutral}</li>
       <li>bad {bad}</li>
+      <li>all {all}</li>
+      <li>average {average}</li>
+      <li>positive {posPercentage}%</li>
     </ul>
   );
 };
